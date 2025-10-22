9edaf85f798d77a637d8073a017c18a895d6ad4d
LENOVO <LENOVO@DESKTOP-VANHGNB>
2025-10-22 20:59:15 -0300

    Adicionar arquivos de projeto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Conmbustivel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace Conmbustivel
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(System.Object sender, System.EventArgs e ) 
+        {
+            try 
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string resultado = "";
+
+                if (etanol <= (gasolina * 0.7))
+                {
+                    resultado = "Etanol esta compenssando. ";
+                }
+                else
+                {
+                    resultado = "Gasolina esta compenssando. ";
+                }
+
+                DisplayAlert("Resultado", resultado, "OK");
+
+
+            } catch (Exception ex) 
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+
+        }
+
+ 
+    }
+}
